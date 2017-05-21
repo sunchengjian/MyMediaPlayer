f2f021c81a1faf1c7904aa99dc6126fc477b44a0
sunchengjian <3230706893@qq.com>
2017-05-21 15:41:22 +0800

    fragment 基类

---
+++ app/src/main/java/com/sunchengjian/mymediaplayer/framgment/BaseFragment.java
@@ -0,0 +1,40 @@
+package com.sunchengjian.mymediaplayer.framgment;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by 0..0 on 2017/5/21.
+ */
+
+public abstract class BaseFragment extends Fragment {
+    private Context context;
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        this.context = context;
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return initView();
+    }
+    public abstract View initView();
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        initData();
+    }
+
+    public void initData() {
+
+    }
+}
