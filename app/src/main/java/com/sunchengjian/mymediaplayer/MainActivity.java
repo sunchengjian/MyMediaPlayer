0321d65514de3bb804e0ec6a973f3e937c9737fb
sunchengjian <3230706893@qq.com>
2017-05-21 16:51:17 +0800

    将本地的所有视频显示出来；6.0以上的动态权限；用到适配器，JavaBean类

--- app/src/main/java/com/sunchengjian/mymediaplayer/MainActivity.java
+++ app/src/main/java/com/sunchengjian/mymediaplayer/MainActivity.java
@@ -1,5 +1,9 @@
 package com.sunchengjian.mymediaplayer;
 
+import android.Manifest;
+import android.app.Activity;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.IdRes;
 import android.support.v4.app.Fragment;
@@ -25,6 +29,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        isGrantExternalRW(this);
 
         rg_main = (RadioGroup) findViewById(R.id.rg_main);
         initFragment();
@@ -87,6 +92,26 @@ public class MainActivity extends AppCompatActivity {
             //把当前的缓存起来
             tempFragment = currentFragment;
         }
+    }
+
+    /**
+     * 解决安卓6.0以上版本不能读取外部存储权限的问题
+     * @param activity
+     * @return
+     */
+    public static boolean isGrantExternalRW(Activity activity) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && activity.checkSelfPermission(
+                Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+
+            activity.requestPermissions(new String[]{
+                    Manifest.permission.READ_EXTERNAL_STORAGE,
+                    Manifest.permission.WRITE_EXTERNAL_STORAGE
+            }, 1);
+
+            return false;
+        }
+
+        return true;
     }
 
 }
--- app/src/main/java/com/sunchengjian/mymediaplayer/pager/LocalVideoPager.java
+++ app/src/main/java/com/sunchengjian/mymediaplayer/pager/LocalVideoPager.java
@@ -3,12 +3,15 @@ package com.sunchengjian.mymediaplayer.pager;
 import android.content.ContentResolver;
 import android.database.Cursor;
 import android.net.Uri;
+import android.os.Handler;
+import android.os.Message;
 import android.provider.MediaStore;
 import android.view.View;
 import android.widget.ListView;
 import android.widget.TextView;
 
 import com.sunchengjian.mymediaplayer.R;
+import com.sunchengjian.mymediaplayer.adapter.LocalVideoAdapter;
 import com.sunchengjian.mymediaplayer.domain.MediaItem;
 import com.sunchengjian.mymediaplayer.framgment.BaseFragment;
 
@@ -22,7 +25,22 @@ public class LocalVideoPager extends BaseFragment {
     private ListView lv;
     private TextView tv_nodata;
     private ArrayList<MediaItem> mediaItems;
+    private LocalVideoAdapter adapter;
 
+    private Handler handler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+            super.handleMessage(msg);
+            if (mediaItems != null & mediaItems.size() > 0) {
+                tv_nodata.setVisibility(View.GONE);
+
+                adapter = new LocalVideoAdapter(context, mediaItems);
+                lv.setAdapter(adapter);
+            } else {
+                tv_nodata.setVisibility(View.VISIBLE);
+            }
+        }
+    };
     @Override
     public View initView() {
         View view = View.inflate(context, R.layout.fragment_local_video_pager, null);
@@ -65,6 +83,8 @@ public class LocalVideoPager extends BaseFragment {
                         String data = cursor.getString(cursor.getColumnIndex(MediaStore.Video.Media.DATA));
 
                         mediaItems.add(new MediaItem(name, duration, size, data));
+                        //子线程不能更新UI 用handler实现
+                        handler.sendEmptyMessage(0);
 
                     }
                     cursor.close();
