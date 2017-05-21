ee4c22ec6cde904dc80a1d8c382640fdcd46c34d
sunchengjian <3230706893@qq.com>
2017-05-21 16:39:47 +0800

    得到本地的所有视频信息，用到JeanBean ，用到数据库查询功能，记得加上读取权限

--- app/src/main/java/com/sunchengjian/mymediaplayer/pager/LocalVideoPager.java
+++ app/src/main/java/com/sunchengjian/mymediaplayer/pager/LocalVideoPager.java
@@ -1,29 +1,75 @@
 package com.sunchengjian.mymediaplayer.pager;
 
-import android.view.Gravity;
+import android.content.ContentResolver;
+import android.database.Cursor;
+import android.net.Uri;
+import android.provider.MediaStore;
 import android.view.View;
+import android.widget.ListView;
 import android.widget.TextView;
 
+import com.sunchengjian.mymediaplayer.R;
+import com.sunchengjian.mymediaplayer.domain.MediaItem;
 import com.sunchengjian.mymediaplayer.framgment.BaseFragment;
+
+import java.util.ArrayList;
 
 /**
  * Created by 0..0 on 2017/5/21.
  */
 
 public class LocalVideoPager extends BaseFragment {
-    private TextView tv;
+    private ListView lv;
+    private TextView tv_nodata;
+    private ArrayList<MediaItem> mediaItems;
 
     @Override
     public View initView() {
-        tv = new TextView(context);
-        tv.setTextSize(30);
-        tv.setGravity(Gravity.CENTER);
-        return tv;
+        View view = View.inflate(context, R.layout.fragment_local_video_pager, null);
+        lv = (ListView) view.findViewById(R.id.lv);
+        tv_nodata = (TextView) view.findViewById(R.id.tv_nodata);
+        return view;
     }
 
     @Override
     public void initData() {
         super.initData();
-        tv.setText("这是本地视频");
+        //加载本地的所有的视屏
+        getData();
+    }
+
+    private void getData() {
+        new Thread() {
+            public void run() {
+                mediaItems = new ArrayList<MediaItem>();
+
+                ContentResolver resolver = context.getContentResolver();
+                Uri uri = MediaStore.Video.Media.EXTERNAL_CONTENT_URI;
+                String[] objs = {
+                        //视频的名字
+                        MediaStore.Video.Media.DISPLAY_NAME,
+                        //视频的时长
+                        MediaStore.Video.Media.DURATION,
+                        //大小
+                        MediaStore.Video.Media.SIZE,
+                        //地址
+                        MediaStore.Video.Media.DATA
+                };
+                Cursor cursor = resolver.query(uri, objs, null, null, null);
+                if (cursor != null) {
+                    while (cursor.moveToNext()) {
+
+                        String name = cursor.getString(cursor.getColumnIndex(MediaStore.Video.Media.DISPLAY_NAME));
+                        long duration = cursor.getLong(cursor.getColumnIndex(MediaStore.Video.Media.DURATION));
+                        long size = cursor.getLong(cursor.getColumnIndex(MediaStore.Video.Media.SIZE));
+                        String data = cursor.getString(cursor.getColumnIndex(MediaStore.Video.Media.DATA));
+
+                        mediaItems.add(new MediaItem(name, duration, size, data));
+
+                    }
+                    cursor.close();
+                }
+            }
+        }.start();
     }
 }
